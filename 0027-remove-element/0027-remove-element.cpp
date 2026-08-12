664694d1ad237e12630f935b51f53ea7175e3559
Ayyan Ahmed <ayyanahmed2356@gmail.com>
2026-08-12 17:37:53 +0500

    Time: 0 ms (100%), Space: 11.6 MB (85.03%) - LeetHub

---
+++ 0027-remove-element/0027-remove-element.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int k = 0;
+        int size = nums.size();
+
+        for(int i = 0; i < size; i++){
+            if(nums[i] != val){
+                nums[k] = nums[i];
+                k+=1;
+            }
+        }
+        return k;
+    }
+};
